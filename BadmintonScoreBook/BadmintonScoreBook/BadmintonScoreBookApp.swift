41b52660cac8f871ec0ee6d4c1bb60613ae2800f
Denny Mathew <devdenmathew@gmail.com>
2025-11-18 19:38:09 +0100

    Adds Google auth

--- BadmintonScoreBook/BadmintonScoreBook/BadmintonScoreBookApp.swift
+++ BadmintonScoreBook/BadmintonScoreBook/BadmintonScoreBookApp.swift
@@ -1,17 +1,27 @@
-//
-//  BadmintonScoreBookApp.swift
-//  BadmintonScoreBook
-//
-//  Created by Denny Mathew on 18.11.25.
-//
-
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct BadmintonScoreBookApp: App {
+        // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
