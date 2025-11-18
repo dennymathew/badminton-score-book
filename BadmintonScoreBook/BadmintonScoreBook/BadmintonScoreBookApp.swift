97327574cb07fefacc4d9f95589f945dcd6c9026
Denny Mathew <devdenmathew@gmail.com>
2025-11-18 19:13:25 +0100

    Adds firebase sdk

---
+++ BadmintonScoreBook/BadmintonScoreBook/BadmintonScoreBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadmintonScoreBookApp.swift
+//  BadmintonScoreBook
+//
+//  Created by Denny Mathew on 18.11.25.
+//
+
+import SwiftUI
+
+@main
+struct BadmintonScoreBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BadmintonScoreBook/BadmintonScoreBookTests/BadmintonScoreBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  BadmintonScoreBookTests.swift
+//  BadmintonScoreBookTests
+//
+//  Created by Denny Mathew on 18.11.25.
+//
+
+import Testing
+@testable import BadmintonScoreBook
+
+struct BadmintonScoreBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
